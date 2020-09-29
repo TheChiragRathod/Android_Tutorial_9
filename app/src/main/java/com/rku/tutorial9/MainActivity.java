a7cf72f4bbd09d70b159e12758ec013a82f56e90
TheChiragRathod <crathod116@rku.ac.in>
2020-09-29 17:50:47 +0530

    added ReadAssets method in MainActivity.java to read json file data.

--- app/src/main/java/com/rku/tutorial9/MainActivity.java
+++ app/src/main/java/com/rku/tutorial9/MainActivity.java
@@ -3,12 +3,70 @@ package com.rku.tutorial9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaration Part start here...
+    EditText Notes;
+    Button SaveNotes;
+    Button ReadNotes;
+    Button ReadAssets;
+    TextView txtHeading;
+    TextView txtData;
+
+    //Declaration Part finished here...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Variables Initialization start here....
+
+        Notes=findViewById(R.id.edtNotes);
+        SaveNotes=findViewById(R.id.btnSaveNotes);
+        ReadNotes=findViewById(R.id.btnReadNotes);
+        ReadAssets=findViewById(R.id.btnReadAssets);
+        txtHeading=findViewById(R.id.txtHeading);
+        txtData=findViewById(R.id.txtData);
+
+        //Variables Initialization finished here....
+    }
+
+
+    public void readAssets(View view)
+    {
+       //get the json file from assets folder
+
+        try {
+
+
+            InputStream is = getAssets().open("data.json");
+
+            //Reading json file.
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+
+            String json = new String(buffer, "UTF-8");
+
+            txtData.setText(json);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
